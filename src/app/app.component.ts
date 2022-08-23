c2526cbedf1b10a8f1fc497fe86485e2a33ceb1d
John Simpson <jsimpson@precisionhealth.net>
2022-08-23 12:51:19 -0400

    Added queryParams and setTimeout on app.component.ts to support dynamic worklist execution.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import {mPageService} from "@clinicaloffice/clinical-office-mpage";
 
 @Component({
@@ -8,11 +9,26 @@ import {mPageService} from "@clinicaloffice/clinical-office-mpage";
 })
 export class AppComponent implements OnInit {
 
-  constructor(public mPage: mPageService) {
-  }
+  constructor(
+    public activatedRoute: ActivatedRoute,
+    public mPage: mPageService
+  ) { }
 
   ngOnInit(): void {
-    this.mPage.setMaxInstances(2, true, 'CHART');
+
+    // Grab any parameters in the URL (Used in Cerner Components)
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.mPage.personId = params.personId ? parseInt(params.personId) : this.mPage.personId;
+      this.mPage.encntrId = params.encounterId ? parseInt(params.encounterId) : this.mPage.encntrId;
+      this.mPage.prsnlId = params.userId ? parseInt(params.userId) : this.mPage.prsnlId;
+    });
+
+    // Perform MPage Initialization
+    setTimeout((e: any) => {
+      this.mPage.setMaxInstances(2, true, 'CHART');
+
+      // Add your initialization code here - do not place outside setTimeout function
+    }, 0);
   }
 
 }
